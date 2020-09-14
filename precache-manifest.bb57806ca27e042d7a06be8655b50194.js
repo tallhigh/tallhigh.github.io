331784a5689ed9a347a1db34e38b03a0e554856d
Talha Turhan <talha.turhan@defacto.com.tr>
2020-09-14 09:54:13 +0300

    Updates

---
+++ precache-manifest.bb57806ca27e042d7a06be8655b50194.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf6823dc0ae4f52b1c7ce4eec2b184f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4429fad46d394b5bde70",
+    "url": "/static/js/2.fd246722.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd246722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c958d8c2962494647c99",
+    "url": "/static/js/main.331c59ff.chunk.js"
+  },
+  {
+    "revision": "a4aa0f6d61ec7306b9ff",
+    "url": "/static/js/runtime-main.1c820cb1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f718909524071535ebf5ef53d7fd46f1.js"
+  "/precache-manifest.bb57806ca27e042d7a06be8655b50194.js"
 );
 
 self.addEventListener('message', (event) => {
