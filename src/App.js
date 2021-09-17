23330eb47cc417f401f7ed86c475a67750ca9cc4
Talha Turhan <talha.turhan@trendyol.com>
2021-09-17 13:40:08 +0300

    Update photo and twitter link

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
             </header>
             <main>
               <div className="container">
-                  <img className="photo" src="/assets/img/talha_turhan_cmp.jpg" alt="Talha Turhan"/>
+                  <img className="photo" src="/assets/img/talha_turhan.jpg" alt="Talha Turhan"/>
                   <h1 className="name">Talha Turhan</h1>
                   <div className="job-info">
                       <span className="position">Software Developer</span>
@@ -18,7 +18,7 @@ function App() {
                   <div className="links">
                       <a href="//github.com/tallhigh" className="github" target="_blank"></a>
                       <a href="//linkedin.com/in/talhaturhan" className="linkedin" target="_blank"></a>
-                      <a href="//twitter.com/tallhighs" className="twitter" target="_blank"></a>
+                      <a href="//twitter.com/_tallhigh" className="twitter" target="_blank"></a>
                       <a href="mailto:info@talhaturhan.com" className="email" target="_blank"></a>
                   </div>
               </div>
